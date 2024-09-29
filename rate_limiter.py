279f7a9a118a21731355bc299ab5e74a7bddda19
Swaroop Srisailam <himasaiswaroop@gmail.com>
2024-09-29 19:04:02 +0530

    Added rate limiter

---
+++ rate_limiter.py
@@ -0,0 +1,16 @@
+from redis_store import track_request, get_request_count
+
+# Defining rate limits for different user tiers as provided in document
+RATE_LIMITS = {
+    "free": 50,     
+    "premium": 200  
+}
+
+def is_rate_limited(api_key: str, user_tier: str) -> bool:
+    """Checks if the API key has exceeded its rate limit."""
+    window = 60  #1 minute
+    limit = RATE_LIMITS[user_tier]  
+    request_count = track_request(api_key, window)  
+    
+    
+    return request_count > limit
